14ed64ce836a48366c37025f7dbe685ecc3dd24c
AAzhar48 <aa48@hood.edu>
2026-02-10 17:47:24 -0500

    MSstrings edited files

---
+++ R/hello.R
@@ -0,0 +1,14 @@
+# hello.R
+# source code for the `hello::hello()`
+
+#' hello
+#' A function to say hello
+#' 
+#' @param name Name of individual(s) to greet
+#' 
+#' @return A character vector of greetings
+#' @export
+hello <- function(name)
+{
+  
+}

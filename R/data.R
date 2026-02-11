eb410a20070912d403d89da2c1c6a5fa867885da
AAzhar48 <aa48@hood.edu>
2026-02-10 20:31:50 -0500

    Data-raw (MSfrags)

---
+++ R/data.R
@@ -0,0 +1,11 @@
+# data.R
+# Documentation for our data
+
+#' MSfrags
+#' Sample peptide fragments in Spectronaut format
+#' 
+#' @format A character vector containing 4 sample peptide fragments 
+#'  with protein modifications in Spectronaut format
+#' @source These fragments were generated from p51681
+#'  (for details, see ...)
+"MSfrags"
